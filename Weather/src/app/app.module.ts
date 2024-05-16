262bb58447ae56ec71833f28dacbaea04624c08e
eg-garus <eg.garus@gmail.com>
2024-05-17 02:44:20 +0400

    New Project12

--- Weather/src/app/app.module.ts
+++ Weather/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {  NgModule } from '@angular/core';
+import { NO_ERRORS_SCHEMA} from '@angular/compiler';
 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   imports: [
     BrowserModule,
     AppRoutingModule
